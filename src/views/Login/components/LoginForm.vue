51f402bf5bb8849d1ab15a1c62acf79353390e0d
Gavin <348453473@qq.com>
2026-04-29 10:43:58 +0400

    fix: enforce dark background for LoginForm inputs

--- src/views/Login/components/LoginForm.vue
+++ src/views/Login/components/LoginForm.vue
@@ -275,7 +275,7 @@ onMounted(() => {
   .loginInput{
     :deep(.el-input__wrapper){
       border: 1px solid rgba(255, 255, 255, 0.2);
-      background: none;
+      background: rgba(0, 0, 0, 0.1) !important;
       border-radius: 32px;
       /* height: 44px; */
       position: relative;
